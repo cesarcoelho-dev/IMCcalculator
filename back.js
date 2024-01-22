a80c096f1b0b7444f3dd01f12760cefa49de8228
cesarcoelho-dev <cesarhenriquecoelho@icloud.com>
2024-01-22 14:46:00 -0300

    parteCerta

---
+++ back.js
@@ -0,0 +1,19 @@
+const calcular = document.getElementById('calcular');
+
+function imc () {
+    
+    const nome = document.getElementById('nome');
+    const altura = document.getElementById('altura');
+    const peso = document.getElementById('peso');
+    const resultado = document.getElementById('resultado');
+
+    if (nome !== '' && altura !== '' && peso !== '') {
+        
+    }else {
+        resultado.textContent = 'Preencha todos os campos'
+    }
+
+}
+
+calcular.addEventListener('click', imc);
+
